8656ccfb4538cb186082205a9185e744b0a27310
JackGrbovic <jman35@hotmail.co.uk>
2025-11-04 21:36:21 +0000

    LoggingTestInLogIn

--- MAE-Frontend/client/src/apiClient.ts
+++ MAE-Frontend/client/src/apiClient.ts
@@ -27,6 +27,8 @@ export const refreshResponse = api.interceptors.request.use(
 async function refresh(error){
     const originalRequest = error.config;
 
+    console.log("originalRequest", originalRequest)
+
         if (error.response?.status === 401 && !originalRequest._retry) {
             originalRequest._retry = true;
 
